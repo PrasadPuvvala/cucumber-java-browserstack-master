3b257b4fd22f832578bb0dda434d6aef78f125ce
iray3 <pssnvprasad@gmail.com>
2022-10-19 17:20:15 +0530

    Merge pull request #3 from PrasadPuvvala/HandleWindow
    file rename

--- cucumber-java-browserstack-master/src/test/java/com/browserstack/stepdefs/ExamCategoriesSteps.java
+++ cucumber-java-browserstack-master/src/test/java/com/browserstack/stepdefs/ExamCategoriesSteps.java
@@ -91,6 +91,8 @@ public class ExamCategoriesSteps {
         /*Handling intermediate window before get into Dashboard*/
     }
 
+    /*Taking Examinations*/
+
     @When("^I take exams")
     public void I_take_exams() throws Throwable
     {
